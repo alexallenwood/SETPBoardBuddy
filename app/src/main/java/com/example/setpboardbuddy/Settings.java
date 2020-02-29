84e438b856997fe76897523e6dc5c706513f047d
up891350 <up891350@myport.ac.uk>
2020-02-29 20:35:41 +0000

    - Configured setting page, started code for colour blind mode and text size - Sorted out a few errors that popped up previously - String file has been updated

--- app/src/main/java/com/example/setpboardbuddy/Settings.java
+++ app/src/main/java/com/example/setpboardbuddy/Settings.java
@@ -2,13 +2,29 @@ package com.example.setpboardbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.os.Bundle;
 
 public class Settings extends AppCompatActivity {
-
+    TextView txt;
+    Button b1,b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        txt = (TextView) findViewById(R.id.textView);
+        b1 = (Button) findViewById(R.id.button);
+        b2 = (Button) findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            b1.setBackgroundColor(Color.parseColor("#236B8E"));
+            b2.setBackgroundColor(Color.parseColor("#236B8E"));
+            txt.setTextColor(Color.parseColor("#FFA500"));
+            }
+        });
     }
 }
