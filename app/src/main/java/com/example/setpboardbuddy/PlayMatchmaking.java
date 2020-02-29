167974fbea3f80a80214bb95d3126aa2dd5c4e28
up899044 <up899044@myport.ac.uk>
2020-02-29 22:17:37 +0000

    - Added some buttons for Play_matchmaking - Made minor changes

--- app/src/main/java/com/example/setpboardbuddy/PlayMatchmaking.java
+++ app/src/main/java/com/example/setpboardbuddy/PlayMatchmaking.java
@@ -2,7 +2,9 @@ package com.example.setpboardbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class PlayMatchmaking extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class PlayMatchmaking extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_matchmaking);
+    }
+
+    public void BackBtnPlay(View view){
+        Intent myIntent = new Intent(PlayMatchmaking.this, MainActivity.class);
+        startActivity(myIntent);
     }
 }
