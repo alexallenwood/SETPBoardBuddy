dab8677d37a1dc78fc9518aee9e4c43987edec64
alexallenwood <alexallenwood@users.noreply.github.com>
2020-03-04 15:42:03 +0000

    Revert "- Added some buttons for Play_matchmaking"
    This reverts commit 167974fbea3f80a80214bb95d3126aa2dd5c4e28.

--- app/src/main/java/com/example/setpboardbuddy/PlayMatchmaking.java
+++ app/src/main/java/com/example/setpboardbuddy/PlayMatchmaking.java
@@ -2,9 +2,7 @@ package com.example.setpboardbuddy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class PlayMatchmaking extends AppCompatActivity {
 
@@ -12,10 +10,5 @@ public class PlayMatchmaking extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_matchmaking);
-    }
-
-    public void BackBtnPlay(View view){
-        Intent myIntent = new Intent(PlayMatchmaking.this, MainActivity.class);
-        startActivity(myIntent);
     }
 }
